01cb36e9c7ae1f5eaf1983afae0d5c89de9692d4
Iniabasi Ekpenyong <iniek@Ini.(none)>
2024-12-12 02:28:06 -0500

    Refined room logic a bit more.

--- attempt2/Room.js
+++ attempt2/Room.js
@@ -1,4 +1,3 @@
-import { Transform3 } from "./Transform3.js";
 import { Renderable } from "./Renderable.js";
 
 
@@ -7,22 +6,20 @@ class Room extends Renderable {
         super(game, transform3, floorGeometry, floorMaterial);
         this.wallMaterial = wallMaterial;
         this.wallGeometry = wallGeometry;
-        this.roomOffset = { x: 0, z: 0 }; // Room's central offset
+        this.objects = [];
+        this.dimensions = {width:50, height:3, depth:50};
+        this.roomOffset = { x: 0, z: 0 };
+        this.ceilingMaterial = floorMaterial;
     }
 
     generate(position) {
-        // Clear old room objects
         this.game.scene.children = this.game.scene.children.filter(obj => obj.type !== "Mesh");
-
-        // Update room offset
         this.roomOffset = position;
-
         // Add Floor
         const floor = new THREE.Mesh(this.geometry, this.material);
-        floor.rotation.x = -Math.PI / 2; // Align horizontally
+        floor.rotation.x = -Math.PI / 2; 
         floor.position.set(position.x, 0, position.z);
         this.game.scene.add(floor);
-
         // Add Walls to Define the Room
         const wallPositions = [
             { x: position.x, y: 1.5, z: position.z - 25 }, // Back wall
@@ -31,9 +28,14 @@ class Room extends Renderable {
             { x: position.x + 25, y: 1.5, z: position.z }  // Right wall
         ];
 
+        const ceiling = new THREE.Mesh(this.geometry, this.ceilingMaterial);
+        ceiling.rotation.x = Math.PI/2;
+        ceiling.position.set(position.x, this.dimensions.height, position.z);
+        this.game.scene.add(ceiling);
+
         wallPositions.forEach(pos => {
             const wall = new THREE.Mesh(this.wallGeometry, this.wallMaterial);
-            wall.scale.set(50, 3, 1); // Scale walls to span the room
+            wall.scale.set(this.dimensions.width, this.dimensions.height, 1); 
             wall.position.set(pos.x, pos.y, pos.z);
             if (pos.x !== position.x) wall.rotation.y = Math.PI / 2; // Rotate side walls
             this.game.scene.add(wall);
@@ -41,27 +43,33 @@ class Room extends Renderable {
 
         console.log(`Room generated at (${position.x}, ${position.z}).`);
     }
-}
 
-// Instantiate Room
-const transform3 = new Transform3(); // Assume Transform3 is defined elsewhere
-const game = { scene: scene }; // Mock game object for context
-const room = new Room(game, transform3, floorMaterial, wallMaterial, floorGeometry, wallGeometry);
+    /**
+     * Add an object to the room.
+     * @param {Renderable} object - An instance of a Renderable object to add.
+     */
+    addObject(object) {
+        this.objects.push(object);
+        this.game.scene.add(object.mesh); // Add object to the scene
+        console.log(`Object added to room at (${object.transform3.pos.x}, ${object.transform3.pos.z}).`);
+    }
 
-// Transition Logic
-function checkRoomTransition() {
-    const roomCenterX = room.roomOffset.x;
-    const roomCenterZ = room.roomOffset.z;
-    const roomSize = 25; // Half the size of the room
+    /**
+     * Remove an object from the room.
+     * @param {Renderable} object - The object to remove.
+     */
+    removeObject(object) {
+        this.objects = this.objects.filter(obj => obj !== object);
+        this.game.scene.remove(object.mesh); // Remove object from the scene
+        console.log(`Object removed from room.`);
+    }
 
-    if (camera.position.z < roomCenterZ - roomSize || camera.position.z > roomCenterZ + roomSize ||
-        camera.position.x < roomCenterX - roomSize || camera.position.x > roomCenterX + roomSize) {
-
-        const newRoomPosition = {
-            x: Math.round(camera.position.x / 50) * 50,
-            z: Math.round(camera.position.z / 50) * 50
-        };
-
-        room.generate(newRoomPosition);
+    /**
+     * Dynamically update the room based on player position or other game logic.
+     * @param {THREE.Vector3} playerPosition - Current player position.
+     */
+    update(playerPosition) {
+        // Example: Add new room generation logic or object spawning logic here
+        console.log(`Updating room relative to player position (${playerPosition.x}, ${playerPosition.z}).`);
     }
 }
