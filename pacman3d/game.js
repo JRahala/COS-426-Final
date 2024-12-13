a17deebdb18dcdb9ff5d3a5d4fe5b62ec1c04a52
Joe Rupertus <joerup2004@gmail.com>
2024-12-13 15:43:26 -0500

    1st person mode

--- pacman3d/game.js
+++ pacman3d/game.js
@@ -13,10 +13,27 @@ class Ghost{
     this.nextPos = new THREE.Vector2(r, c);
     this.nextDir = new THREE.Vector2(dr, dc);
 
+    this.ro = r;
+    this.co = r;
+
     this.state = 0; // 0 = scatter, 1 = chase, 2 = frightened
     // scatter target tiles
     this.str = str;
     this.stc = stc;
+  }
+
+  reset(){
+      this.r = this.ro;
+      this.c = this.co;
+      this.dr = 0;
+      this.dc = 0;
+      this.pellets = 0;
+      
+      this.position = new THREE.Vector2(this.r, this.c);
+      this.orientation = 0;
+
+      this.nextPos = new THREE.Vector2(this.r, this.c);
+      this.nextDir = new THREE.Vector2(this.dr, this.dc);
   }
 
   frightenedChoice(moves){
@@ -129,7 +146,15 @@ class Game{
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
       ];
 
-      constructor(){
+      constructor(scene){
+        this.scene = scene;
+
+        this.score = 0;
+        this.isOver = false
+
+        this.pellets = [];
+        this.powerPellets = [];
+
         this.resetGame();
       }
 
@@ -142,6 +167,9 @@ class Game{
         this.player = new Player(7, 10, 1, 0);
         this.ghosts = [redGhost, pinkGhost, blueGhost, orangeGhost];
         this.maze = this.resetMaze();
+        this.isOver = false;
+
+        this.resetScore();
       }
 
       resetMaze(){
@@ -184,7 +212,7 @@ class Game{
             return 
           }
         }
-        
+
         this.player.position.x = nx;
         this.player.position.y = ny;
 
@@ -258,6 +286,58 @@ class Game{
             this.ghosts[3].tc = this.ghosts[3].stc;
         }
       }
+      
+      checkGhostCollision(){
+        for (const ghost of this.ghosts){
+            if (Math.abs(ghost.r - this.player.r) < 1 && Math.abs(ghost.c - this.player.c) < 1){
+              if (ghost.state == 2){
+                ghost.reset()
+                this.updateScore(200);
+              } else {
+                const deathMessage = document.getElementById("death-message");
+                deathMessage.style.display = "flex";
+                this.isOver = true;
+              }
+              return;
+            }
+        }
+      }
+
+      checkPelletConsumption() {
+        const pelletConsumptionRadius = 0.7071; // allows for diagonal passes
+
+        for (let i = this.pellets.length - 1; i >= 0; i--) {
+            const pellet = this.pellets[i];
+            if (Math.abs(player.position.x - pellet.position.x) < pelletConsumptionRadius &&
+                Math.abs(player.position.z - pellet.position.z) < pelletConsumptionRadius) {
+                scene.remove(pellet); 
+                this.pellets.splice(i, 1); 
+                this.updateScore(10);
+            }
+        }
+
+        for (let i = this.powerPellets.length - 1; i >= 0; i--) {
+            const powerPellet = this.powerPellets[i];
+            if (Math.abs(player.position.x - powerPellet.position.x) < pelletConsumptionRadius &&
+                Math.abs(player.position.z - powerPellet.position.z) < pelletConsumptionRadius) {
+                scene.remove(powerPellet); 
+                this.powerPellets.splice(i, 1);
+                this.updateScore(50);
+            }
+        }
+      }
+
+      updateScore(amount){
+        this.score += amount 
+        const scoreElement = document.getElementById("score");
+        scoreElement.textContent = this.score;
+      }
+
+      resetScore(){
+        this.score = 0;
+        const scoreElement = document.getElementById("score");
+        scoreElement.textContent = this.score;
+      }
 }
 
 
@@ -270,6 +350,7 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+let isFirstPersonView = true;
 
 // Add a basic light
 const light = new THREE.AmbientLight(0x404040, 2); // Soft white light
@@ -297,10 +378,10 @@ const createWall = (x, y, z) => {
     return wall;
 };
 
-
+const size = 0.4;
 // Create player and ghost placeholders
 const createSphere = (color, x, y, z) => {
-    const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
+    const sphereGeometry = new THREE.SphereGeometry(size, 32, 32);
     const sphereMaterial = new THREE.MeshStandardMaterial({ color });
     const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
     sphere.position.set(x, y, z);
@@ -313,10 +394,10 @@ scene.add(player);
 
 // Ghosts
 const ghosts = [
-    createSphere(0xff0000, 15, 0.5, 12), // Red ghost
-    createSphere(0xffb6c1, 1, 0.5, 7),  // Pink ghost
-    createSphere(0x0000ff, 1, 0.5, 10), // Blue ghost
-    createSphere(0xffa500, 15, 0.5, 14) // Orange ghost
+    createSphere(0xff0000, 15, size, 12), // Red ghost
+    createSphere(0xffb6c1, 1, size, 7),  // Pink ghost
+    createSphere(0x0000ff, 1, size, 10), // Blue ghost
+    createSphere(0xffa500, 15, size, 14) // Orange ghost
 ];
 ghosts.forEach(ghost => scene.add(ghost));
 
@@ -333,24 +414,16 @@ const createPellet = (x, z, isPowerUp = false) => {
   return pellet;
 };
 
-// Arrays to store pellets and power-up pellets
-const pellets = [];
-const powerPellets = [];
-
-
-
-// Position the camera
-// camera.position.set(19/2-1/2, 25, 25);
-// camera.lookAt(19/2-1/2, 0, 0);
-camera.position.set(19/2-1/2, 15, 20);
-camera.lookAt(19/2-1/2, -10, 0);
-
 // Animate the scene
 const animate = () => {
     requestAnimationFrame(animate);
 
+    if (G.isOver) {
+      return;
+    }
+
     const movementSpeed = 0.03;
-    const rotationSpeed = 0.05;
+    const rotationSpeed = 0.03;
 
     // Handle player movement based on key state
     if (keys.w || keys.ArrowUp) G.movePlayer(movementSpeed);
@@ -360,32 +433,15 @@ const animate = () => {
 
     // Move the ghosts
     G.moveGhosts(movementSpeed);
+
+    // Update the ghost targets
     G.updateGhostTargets();
 
-    // Rotate pellets for a dynamic effect
-    pellets.forEach(pellet => pellet.rotation.y += 0.03);
-    powerPellets.forEach(powerPellet => powerPellet.rotation.y += 0.03);
+    // Check for ghost collision
+    G.checkGhostCollision();
 
     // Check for pellet consumption
-    const pelletConsumptionRadius = 0.7071; // allows for diagonal passes
-    for (let i = pellets.length - 1; i >= 0; i--) {
-        const pellet = pellets[i];
-        if (Math.abs(player.position.x - pellet.position.x) < pelletConsumptionRadius &&
-            Math.abs(player.position.z - pellet.position.z) < pelletConsumptionRadius) {
-            scene.remove(pellet); // Remove the pellet from the scene
-            pellets.splice(i, 1); // Remove it from the array
-        }
-    }
-
-    for (let i = powerPellets.length - 1; i >= 0; i--) {
-        const powerPellet = powerPellets[i];
-        if (Math.abs(player.position.x - powerPellet.position.x) < pelletConsumptionRadius &&
-            Math.abs(player.position.z - powerPellet.position.z) < pelletConsumptionRadius) {
-            scene.remove(powerPellet); // Remove the power-up pellet
-            powerPellets.splice(i, 1);
-            // Add power-up effect here if needed
-        }
-    }
+    G.checkPelletConsumption();
 
     // Update positions based on the game state
     player.position.set(G.player.position.y, 0, G.player.position.x);
@@ -393,6 +449,20 @@ const animate = () => {
     G.ghosts.forEach((g, i) => {
         ghosts[i].position.set(g.position.y, 0, g.position.x);
     });
+
+    // Rotate pellets for a dynamic effect
+    G.pellets.forEach(pellet => pellet.rotation.y += 0.03);
+    G.powerPellets.forEach(powerPellet => powerPellet.rotation.y += 0.03);
+
+    // Position the camera
+    if (isFirstPersonView) {
+        const angle = Math.PI + G.player.orientation;
+        camera.position.set(G.player.position.y + 2 * Math.sin(angle), 1, G.player.position.x + 2 * Math.cos(angle));
+        camera.lookAt(G.player.position.y, 0, G.player.position.x);
+    } else {
+        camera.position.set(19 / 2 - 1 / 2, 15, 20);
+        camera.lookAt(19 / 2 - 1 / 2, -10, 0);
+    }
 
     renderer.render(scene, camera);
 };
@@ -438,6 +508,11 @@ window.addEventListener('keydown', (event) => {
         ghost.state = 2;
       }
     }
+
+    if (event.key === 'v') { // Press 'V' to toggle view mode
+      console.log(`View mode: ${isFirstPersonView ? 'First-Person' : 'Overhead'}`);
+      isFirstPersonView = !isFirstPersonView;
+    }
 });
 
 window.addEventListener('keyup', (event) => {
@@ -446,9 +521,15 @@ window.addEventListener('keyup', (event) => {
     }
 });
 
+const respawnButton = document.getElementById("respawn-button");
+respawnButton.addEventListener("click", () => {
+    const deathMessage = document.getElementById("death-message");
+    deathMessage.style.display = "none"; 
+    G.resetGame(); 
+});
 
 
-const G = new Game();
+const G = new Game(scene);
 
 // Generate walls and add to the scene
 const maze = G.maze;
@@ -467,11 +548,11 @@ for (let r = 0; r < maze.length; r++) {
   for (let c = 0; c < maze[r].length; c++) {
       if (maze[r][c] === 2) { // Regular pellets
           const pellet = createPellet(c, r, false);
-          pellets.push(pellet);
+          G.pellets.push(pellet);
           scene.add(pellet);
       } else if (G.maze[r][c] === 3) { // Power-up pellets
           const powerPellet = createPellet(c, r, true);
-          powerPellets.push(powerPellet);
+          G.powerPellets.push(powerPellet);
           scene.add(powerPellet);
       }
   }
